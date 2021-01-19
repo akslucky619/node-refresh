4227419052fed32033aa0a52d2bc81252fb9ab1d
akash@619 <Akash.sharma1@in.ey.com>
2021-01-19 21:15:32 +0530

    add express

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-const http = require("http");
+const express = require("express");
 
-const fs = require("fs");
+const app = express();
 
-const homePage = fs.readFileSync("index.html");
-const myassPage = fs.readFileSync("myass.html");
-const yourassPage = fs.readFileSync("yourass.html");
-const ourassPage = fs.readFileSync("ourass.html");
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/myass") {
-    return res.end(myassPage);
-  } else if (req.url === "/yourass") {
-    return res.end(yourassPage);
-  } else if (req.url === "/ourass") {
-    return res.end(ourassPage);
-  } else if (req.url === "/") {
-    return res.end(homePage);
-  } else {
-    res.writeHead(404);
-
-    res.end("ASSES NOT FOUND");
-  }
+app.get("/", (req, res) => {
+  res.send({
+    name: "home page",
+  });
 });
 
-server.listen(3000);
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
+
+// const http = require("http");
+
+// const fs = require("fs");
+
+// const homePage = fs.readFileSync("index.html");
+// const myassPage = fs.readFileSync("myass.html");
+// const yourassPage = fs.readFileSync("yourass.html");
+// const ourassPage = fs.readFileSync("ourass.html");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/myass") {
+//     return res.end(myassPage);
+//   } else if (req.url === "/yourass") {
+//     return res.end(yourassPage);
+//   } else if (req.url === "/ourass") {
+//     return res.end(ourassPage);
+//   } else if (req.url === "/") {
+//     return res.end(homePage);
+//   } else {
+//     res.writeHead(404);
+
+//     res.end("ASSES NOT FOUND");
+//   }
+// });
+
+// server.listen(3000);
